10380d018eefc238b53b1a4c1bf2d4cfa3a8eccb
Kamba1a <headcr@bk.ru>
2019-02-09 14:08:50 +0300

    Урок 5

---
+++ Lesson5/Task1_checkLogin/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+//ОЛЬГА НАЗАРОВА
+//1.
+//Создать программу, которая будет проверять корректность ввода логина.
+//Корректным логином будет строка от 2 до 10 символов,
+//содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+//а) без использования регулярных выражений;
+//б) ** с использованием регулярных выражений.
+
+namespace Task1_checkLogin
+{
+    class Program
+    {
+        #region Методы для выполения пункта а) задачи
+
+        /// <summary>
+        /// Проверка символа, является ли он латинской буквой
+        /// </summary>
+        /// <param name="symbol">символ</param>
+        /// <returns></returns>
+        public static bool checkIsLatinLetter(char symbol)
+        {
+            int symbolCode = symbol;
+            return symbolCode >= 97 && symbolCode <= 122 || symbolCode >= 65 && symbolCode <= 90;
+        }
+
+        /// <summary>
+        /// Метод проверки логина на соответствие условию задачи
+        /// </summary>
+        /// <param name="login">логин</param>
+        /// <returns></returns>
+        public static bool checkLogin (string login)
+        {
+            if (login.Length >= 2 && login.Length <= 10 && checkIsLatinLetter(login[0]))
+            {
+                for (int i = 1; i < login.Length; i++)
+                {
+                    if (!(checkIsLatinLetter(login[i]) || char.IsNumber(login[i])))
+                    {
+                        return false;
+                    }
+                }   
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Введите логин:");
+                string login = Console.ReadLine();
+
+                #region а) Проверка логина без использования регулярных выражений
+                if (checkLogin(login))
+                #endregion
+
+                #region б) Проверка логина с использованием регулярных выражений
+                //Regex loginPattern = new Regex("^[a-zA-Z]{1}[a-zA-Z0-9]{1,9}$");
+                //if (loginPattern.IsMatch(login))
+                #endregion
+
+                {
+                    Console.WriteLine("Логин принят\n");
+                }
+                else
+                {
+                    Console.WriteLine("Логин не корректен\n");
+                }
+            }
+        }
+    }
+}
---
+++ Lesson5/Task2_messageClass/Message.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//ОЛЬГА НАЗАРОВА
+//2. Разработать статический класс ​ Message​ , содержащий следующие статические методы для обработки текста:
+//а) Вывести только те слова сообщения, которые содержат не более n букв.
+//б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+//в) Найти самое длинное слово сообщения.
+//г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+//д) *** Создать метод, который производит частотный анализ текста.
+//  В качестве параметра в него передается массив слов и текст, в качестве результата метод возвращает сколько раз
+//  каждое из слов массива входит в этот текст. Здесь требуется использовать класс Dictionary.
+
+namespace Task2_messageClass
+{
+    public static class Message
+    {   
+
+        /// <summary>
+        /// Метод создания массива из слов текса
+        /// </summary>
+        /// <param name="msg">текст</param>
+        /// <returns></returns>
+        static string[] stringToArray (string msg)
+        {
+            Regex regex = new Regex(@"\w+");
+            string[] msgArray = new string[regex.Matches(msg).Count];
+            int i = 0;
+            foreach (Match word in regex.Matches(msg))
+            {
+                msgArray[i] = word.ToString();
+                i++;
+            }
+            return msgArray;
+        }
+
+        /// <summary>
+        /// а) Вывести только те слова сообщения, которые содержат не более n букв.
+        /// </summary>
+        /// <param name="msg">сообщение</param>
+        /// <param name="wordLength">максимальная длина слова</param>
+        public static void maxWordSize(string msg, int wordLength)
+        {
+            string[] msgArray = stringToArray(msg);
+
+            for (int i = 0; i < msgArray.Length; i++)
+            {
+                if (msgArray[i].Length <= wordLength)
+                {
+                    Console.Write($"{msgArray[i]} ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        /// (возвращает новую строку)
+        /// </summary>
+        /// <param name="msg">сообщение</param>
+        /// <param name="symbol">символ</param>
+        /// <returns>возвращает новую строку</returns>
+        public static string wordRemove(string msg, char symbol)
+        {
+            string[] msgArray = stringToArray(msg);
+            string newMSg = "";
+
+            for (int i = 0; i < msgArray.Length; i++)
+            {
+                if (msgArray[i][msgArray[i].Length - 1] != symbol)
+                {
+                    newMSg = newMSg + msgArray[i] + " ";
+                }
+            }
+            newMSg.TrimEnd(' ');
+            return newMSg;
+        }
+
+        /// <summary>
+        /// б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        /// (меняет само сообщение)
+        /// </summary>
+        /// <param name="msg">сообщение</param>
+        /// <param name="symbol">символ</param>
+        public static void wordRemove(ref string msg, char symbol)
+        {
+            string[] msgArray = stringToArray(msg);
+            for (int i = 0; i < msgArray.Length; i++)
+            {
+                if (msgArray[i][msgArray[i].Length - 1] == symbol)
+                {
+                    msg.Replace(msgArray[i], "");
+                }
+            }
+        }
+
+        /// <summary>
+        /// //в) Найти самое длинное слово сообщения.
+        /// </summary>
+        /// <param name="msg">сообщение</param>
+        /// <returns></returns>
+        public static string maxWord (string msg)
+        {
+            string[] msgArray = stringToArray(msg);
+            int max = msgArray[0].Length;
+            string word = msgArray[0];
+
+            for (int i = 1; i < msgArray.Length; i++)
+            {
+                if (msgArray[i].Length > max)
+                {
+                    max = msgArray[i].Length;
+                    word = msgArray[i];
+                }
+            }
+            return word;
+        }
+
+        /// <summary>
+        /// г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+        /// </summary>
+        /// <param name="msg">сообщение</param>
+        /// <returns></returns>
+        public static string maxWordString (string msg)
+        {
+            StringBuilder maxWords = new StringBuilder();
+            string[] msgArray = stringToArray(msg);
+            int max = msgArray[0].Length;
+            for (int i = 1; i < msgArray.Length; i++)
+            {
+                if (msgArray[i].Length > max)
+                {
+                    max = msgArray[i].Length;
+                }
+            }
+            for (int i = 0; i < msgArray.Length; i++)
+            {
+                if (msgArray[i].Length == max)
+                {
+                    maxWords.Append(msgArray[i]);
+                    maxWords.Append(" ");
+                }
+            }
+            return maxWords.ToString().TrimEnd(' ');
+        }
+
+        /// <summary>
+        /// ***Создать метод, который производит частотный анализ текста.
+        /// требуется использовать класс Dictionary.
+        /// </summary>
+        /// <param name="array">массив слов</param>
+        /// <param name="text">анализируемый текст</param>
+        public static void textAnalysis(string[] array, string text)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {   
+                Regex regex = new Regex(array[i]);
+                dict[array[i]] = regex.Matches(text.ToLower()).Count;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]} - {dict[array[i]]}");
+            }
+        }
+
+    }
+}
